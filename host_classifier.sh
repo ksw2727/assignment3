5476b8c03f2d5ebf02b8f156c5af59ec26bf5caa
Kevin Whelan <Kevin.Whelan@encore.tech>
2019-06-05 17:16:26 -0400

    Made corrections to the file so Nick will <3 it...a lot!

--- host_classifier.sh
+++ host_classifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#assignment #3
+#assignment #3 ...rename to host_classifier.sh
 #This script should accept one or more filenames as arguments
 #The files will be stored in my home directory
 # /home/ipa/corp.encore.tech/kevin.whelan/files/assignment3/*
@@ -14,9 +14,14 @@
 #All Files using wildcard only
 #host_classifier.sh ~/files/assignment3/*
 
-#This varible will be used to print the filenames without the directory
 
-#####Initialize Variables#####
+#####Primary Script#####
+
+#1stWhile
+#The "1stWhile" loop shifts to each file input addressing the multiple files requirement.
+while [ "$1" != "" ]
+ do
+ #####Initialize Variables#####
  prod_env_count=0
  dev_env_count=0
  other_env_count=0
@@ -25,29 +30,23 @@
  lin_os_count=0
  other_os_count=0
 
-#####Primary Script#####
 
-#1stWhile
-#The "1stWhile" loop shifts to each file input addressing the multiple files requirement.
-while [ "$1" != "" ]
- do
  #prints the current filename excluding the path.  I'm sure there's a better way.
   echo =========================
   filename=$(echo $1 | cut --delimiter "/" --fields 8)
   echo "File =" $filename
-  echo "Containing servers named:"
-  echo =========================
+#  echo "Containing servers named:"
+#  echo =========================
 
   #2ndWhile
-  #The "2ndWhile" we read and print each line of current file input from when we ran script ... 
+  #The "2ndWhile" we read and print each line of current file input from when we ran script ...
   #... and then handle the various OS and Environment counts with "cases".
 
   while IFS= read -r line
    do
-    echo "$line"
 
-    #1stCase   
-    #For each hostname you should classify the host by environment: 
+    #1stCase
+    #For each hostname you should classify the host by environment:
     #Environment = Production
     #Environment = Development
     #Environment = Other (if it isn't Production or Development)
@@ -84,15 +83,15 @@ while [ "$1" != "" ]
   done < "$1"
 
 shift
-done
+
 
 #####Results#####
 #Print out the total counts for environment type and os type for this file in the following format
-  echo ===Env Count All Files====
-  echo "Prod  env Count =" $prod_env_count
-  echo "Dev   env Count =" $dev_env_count
-  echo "Other env Count =" $other_env_count
-  echo ===OS  Count All Files====
-  echo "Win   os  Count =" $win_os_count
-  echo "Linux os  Count =" $lin_os_count
-  echo "Other os  Count =" $other_os_count
+  echo "OS Windows        =" $win_os_count
+  echo "OS Linux          =" $lin_os_count
+  echo "OS Other          =" $other_os_count
+  echo "Environment Prod  =" $prod_env_count
+  echo "Environment Dev   =" $dev_env_count
+  echo "Environment Other =" $other_env_count
+
+done
